97b71eee811cd1ed2f62f5b74a6db290ce12f4bb
mohammad-anash <anash40678910@gmail.com>
2023-12-19 17:22:23 +0530

    coding practise question

---
+++ day1.js
@@ -0,0 +1,16 @@
+// function addition(a, b) {
+//   return +a + +b;
+// }
+
+// console.log(addition(3, 2));
+// console.log(addition(-3, -6));
+// console.log(addition(7, 3));
+// console.log(addition("7", "3"));
+// console.log(addition("7", 3));
+// console.log(addition("7.5", 3));
+
+
+// convert(5) ➞ 300
+//   convert(3) ➞ 180
+//   convert(2) ➞ 120
+//   convert('2') ➞ 120
